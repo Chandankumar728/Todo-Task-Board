381739d3252af8f70235722c5f9afa197822d5e4
Chandankumar728 <chandankumar6299068@gmail.com>
2025-08-05 13:50:08 +0530

    added vercel.json for deployement purpose

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1LpJHYQ1kU8IBfgrUmw4gnHAqryDtF8A",
-  authDomain: "task-board-5930f.firebaseapp.com",
-  projectId: "task-board-5930f",
-  storageBucket: "task-board-5930f.firebasestorage.app",
-  messagingSenderId: "725110766019",
-  appId: "1:725110766019:web:3f4941983b721b1f13a576",
-  measurementId: "G-WVPVDEZRX0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
